26721a352e2e75eb2ed86914a0bee1f9b642bf30
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-04-28 23:44:11 +0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Hina Khan on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
