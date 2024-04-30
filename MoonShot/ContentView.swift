cc74ed096361b7d73b52893bfcd558b4b9987b01
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-04-30 23:38:52 +0500

    Resize images to fit the available space

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.example)
+                .frame(width: 300, height: 300)    .clipped()
+
         }
         .padding()
     }
