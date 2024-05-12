fba043c34bad5b0b8456258df9bcbc7477e4bd5e
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-05-12 22:48:11 +0500

    lay out views in a scrolling grid

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -17,23 +17,19 @@ struct Address: Codable {
     let city: String
 }
 struct ContentView: View {
+    
+    let layout = [
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80))
+    ]
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }
