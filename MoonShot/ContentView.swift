9de7b145f70d51a560fe74a8d57da12ac818b959
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-05-18 23:54:56 +0500

    Using generics to load any kind of Codable data

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -19,7 +19,8 @@ struct Address: Codable {
 
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
         Text(String(astronauts.count))
