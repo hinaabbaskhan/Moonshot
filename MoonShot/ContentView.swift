9c87346db9891a58d72ea38bd00c91f9f5ee30e1
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-05-19 22:08:43 +0500

    parse dates in the exact format we expect

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -12,35 +12,40 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            Text("Detail view")
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
+                    ScrollView {
+                        LazyVGrid(columns: columns) {
+                            ForEach(missions) { mission in
+                                NavigationLink {
+                                    Text("Detail view")
+                                } label: {
+                                    VStack {
+                                        Image(mission.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 100, height: 100)
 
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                    Text(mission.launchDate ?? "N/A")
-                                        .font(.caption)
+                                        VStack {
+                                            Text(mission.displayName)
+                                                .font(.headline)
+                                            Text(mission.formattedLaunchDate )
+                                                .font(.caption)
+                                        }
+                                        .frame(maxWidth: .infinity)
+                                    }
                                 }
                                 .frame(maxWidth: .infinity)
                             }
                         }
                     }
+                    .navigationTitle("Moonshot")
                 }
-            }
-            .navigationTitle("Moonshot")
-        }
 //        Text(String(astronauts.count))
 
     }
