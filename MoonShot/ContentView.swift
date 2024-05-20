4b2cdb309c1bb0edb5153a3a5a992939da0215f3
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-05-20 17:53:59 +0500

    add a toolbar item to ContentView that toggles between showing missions as a grid and as a list.

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,58 +7,131 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    @State private var showingGrid = true
 
+    var body: some View {
+        NavigationStack {
+            Group {
+                if showingGrid {
+                    GridLayout(astronauts: astronauts, missions: missions)
+                } else {
+                    ListLayout(astronauts: astronauts, missions: missions)
+                }
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .toolbar {
+               ToolbarItem(placement: .navigationBarTrailing) {
+                   Button(action: {
+                       showingGrid.toggle()
+                   }) {
+                       Image(systemName: showingGrid ? "list.bullet" : "square.grid.2x2")
+                   }
+               }
+           }
+        }
+    }
+}
+
+
+struct GridLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                
+                                Text(mission.formattedLaunchDate )
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.5))
+                                
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                            .frame(maxWidth: .infinity)
+                        }
+                        .clipShape(.rect(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            
+        }
+    }
+    
+    
+}
+
+struct ListLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
     
     var body: some View {
-        NavigationStack {
-                    ScrollView {
-                        LazyVGrid(columns: columns) {
-                            ForEach(missions) { mission in
-                                NavigationLink {
-                                    MissionView(mission: mission, astronauts: astronauts)
-                                } label: {
-                                    VStack {
-                                        Image(mission.image)
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 100, height: 100)
-                                            .padding()
-
-                                        VStack {
-                                            Text(mission.displayName)
-                                                .font(.headline)
-                                                .foregroundStyle(.white)
-                                            Text(mission.formattedLaunchDate )
-                                                .font(.caption)
-                                                .foregroundStyle(.white.opacity(0.5))
-                                        }
-                                        .padding(.vertical)
-                                        .frame(maxWidth: .infinity)
-                                        .background(.lightBackground)
-                                        .frame(maxWidth: .infinity)
-                                    }
-                                    .clipShape(.rect(cornerRadius: 10))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(.lightBackground)
-                                    )
-                                }
-                            }
+            List(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission,astronauts: astronauts)
+                }label: {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        
+                        
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Text(mission.formattedLaunchDate )
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                            
                         }
-                        .padding([.horizontal, .bottom])
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                        .frame(maxWidth: .infinity)
+                        
                     }
-                    .navigationTitle("Moonshot")
-                    .background(.darkBackground)
-                    .preferredColorScheme(.dark)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
                 }
+            }
+            .listStyle(.plain)
     }
 }
 
